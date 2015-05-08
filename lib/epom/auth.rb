008b19d4330249b0ae868f60eaf0ee666e872f3d
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-05-08 11:00:15 -0400

    auth.rb and auth_test.rb REFACTORED 3 runs, 18 assertions, 0 failures, 0 errors, 0 skips

--- lib/epom/auth.rb
+++ lib/epom/auth.rb
@@ -3,26 +3,27 @@ require 'epom/epom_element'
 module Epom
   class Auth < EpomElement
 
-    def self.extended_parameters
+    def self.extended_methods
       {
           :get_authentication_token => {
               :url => '/rest-api/auth/token.do',
-              :parameters => [:username, :password],
+              :body_parameters => [:username, :password],
               :method => :post
           },
           :register_user => {
               :url => '/rest-api/register-user/KEY/HASH/TIMESTAMP.do',
-              :parameters => [:key, :hash, :timestamp, :username, :password, :email, :role, :phone, :firstName, :lastName, :websiteUrl, :company, :country, :state, :enable_market_integration],
+              :url_parameters => [:key, :hash, :timestamp],
+              :body_parameters => [:username, :password, :email, :role, :phone, :firstName, :lastName, :websiteUrl, :company, :country, :state, :enable_market_integration],
               :method => :post
           }
       }
     end
 
-    def self.replace_string_identifiers(url, params)
-      url.gsub!('TOKEN', params[:token]) if url.include?('TOKEN')
-      url.gsub!('KEY', params[:key]) if url.include?('KEY')
-      url.gsub!('HASH', params[:hash]) if url.include?('HASH')
-      url.gsub!('TIMESTAMP', params[:timestamp].to_s) if url.include?('TIMESTAMP')
+    def self.replace_params_in_url(url, url_params)
+      url.gsub!('TOKEN', url_params[:token]) if url.include?('TOKEN')
+      url.gsub!('KEY', url_params[:key]) if url.include?('KEY')
+      url.gsub!('HASH', url_params[:hash]) if url.include?('HASH')
+      url.gsub!('TIMESTAMP', url_params[:timestamp].to_s) if url.include?('TIMESTAMP')
       url
     end
 
