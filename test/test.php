fd0feb08a80e98038c58cb5f89ba89aba6b3cd52
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-05-14 16:31:56 -0400

    testing php script creating banner with LOCAL_FILE

--- test/test.php
+++ test/test.php
@@ -1,39 +1,51 @@
 <?php
-	/*Initiating username and password variables, generating current timestamp and hash*/
+	define("DEBUG",false);
+	
 	$username = "kewelta";
 	$password = "kewelta";	
 	$timestamp  = round(microtime(true) * 1000);
 	$hash = md5(md5($password).$timestamp);
-
-	/*Specifying API call URL*/
-	$url = "https://n29.epom.com/rest-api/sites/721/pricing.do";
+		
+	$url = "https://n29.epom.com/rest-api/banner/create.do";	
+	/*Getting the full path to your file*/
+	$file_name_with_full_path = realpath('300x250.gif');
+	/*Specifying MIME type of the file*/
+	$filetype = "image/gif";
 	
-	/*Specifying request parameters*/
 	$post_data = array(
 		"hash" => $hash,
 		"timestamp" => $timestamp,
-		"username" => $username
+		"username" => $username,
+		"placementType" => "SITE_PLACEMENT",
+		"campaignId" => "1261",
+		"active" => "true",
+		"name" => "test_banner_1",
+		"weight" => "1",
+		"bannerType" => "LOCAL_FILE",
+		"imageBannerLink" => "http://qwe.com",
+		"imageFile" => "@$file_name_with_full_path;type=$filetype",
+		"url" => "http://qwe.com",
+		"adUnitId" => "1"
 	);
-
-	$json = "{\"paymentModel\":\"FIXED_PRICE\",\"pricingType\":\"CPM\",\"price\":2.0}";
-
-	/*specifying curl options*/
+	
 	$options = array(
-	  CURLOPT_URL => $url . "?" . http_build_query($post_data),
-	  CURLOPT_PROXY => '10.6.56.19:808',
-	  CURLOPT_SSL_VERIFYPEER => false,
-	  CURLOPT_POST => true, // POST method is used
-	  CURLOPT_POSTFIELDS => $json, //POST request body parameters
-	  CURLOPT_HTTPHEADER => array('Content-type: application/json'), //This header is mandatory in case if parameters are passed in request body.
-	  CURLOPT_RETURNTRANSFER => true
-	 );
-
-	/*connection initiation*/
+		CURLOPT_URL => $url,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_POST => true,		
+		CURLOPT_POSTFIELDS => $post_data, 
+		CURLOPT_HTTPHEADER => array("Content-type: multipart/form-data"),		
+		CURLOPT_RETURNTRANSFER => true
+	);
+	
 	$curl = curl_init();
-	/*Applying curl options to our curl instance*/
-	curl_setopt_array($curl,$options);
-	/*Executing the call*/	
-	$result = curl_exec($curl);
+	
+	curl_setopt_array($curl,$options);	
 
-	echo $result;	
+	echo curl_exec($curl);
+	
+	if(DEBUG){
+		echo "\n\n";
+		echo $url;
+		print_r($post_data);
+	}		
 ?>
