d5efc1fc8773cb207d9a003dffbb3287615751d1
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-18 15:02:53 -0400

    def set_environment_variables   ENV['advertiser_id'] = create_advertiser.to_s   ENV['campaign_id'] = create_campaign.to_s   ENV['banner_id'] = create_banner.to_s
      ENV['site_id'] = create_site.to_s
      ENV['zone_id'] = create_zone.to_s
      ENV['placement_id'] = create_standard_placement.to_s
    
      ENV['banner_type'] = 'LOCAL_FILE'
      ENV['target_id'] = '4034'
      ENV['os_name'] = 'UNSPECIFIED'
      ENV['placement_type'] = 'SITE_PLACEMENT'
      ENV['country_code'] = 'US'
      ENV['region_name'] = 'New York'
      ENV['action_key'] = 'cd6091a6b570ac1a0f5d9e0d396220fb-526-1247'
    end

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -57,4 +57,98 @@ def define_get_tests_auto(klass)
 	end
 end
 
+def set_environment_variables
+  ENV['advertiser_id'] = create_advertiser.to_s
+  ENV['campaign_id'] = create_campaign.to_s
+  ENV['banner_id'] = create_banner.to_s
+
+  ENV['site_id'] = create_site.to_s
+  ENV['zone_id'] = create_zone.to_s
+  ENV['placement_id'] = create_standard_placement.to_s
+  
+  ENV['banner_type'] = 'LOCAL_FILE'
+  ENV['target_id'] = '4034'
+  ENV['os_name'] = 'UNSPECIFIED'
+  ENV['placement_type'] = 'SITE_PLACEMENT'
+  ENV['country_code'] = 'US'
+  ENV['region_name'] = 'New York'
+  ENV['action_key'] = 'cd6091a6b570ac1a0f5d9e0d396220fb-526-1247'
+end
+
+def create_advertiser
+  body_params = {
+    :name => "advertiser #{DateTime.now}",
+    :contactName => "advertiser contactName",
+    :contactEmail => "advertiser@gmail.com",
+    :description => "advertiser description"
+  }
+  response = Epom::Advertiser.create_advertiser({}, body_params)
+  response['id']
+end
+
+def create_campaign
+  body_params = {
+    :advertiserId => ENV['advertiser_id'], 
+    :name => "campaign #{timestamp}",
+    :description => "description for campaign #{timestamp}",
+    :active => true
+  }
+  response = Epom::Campaign.create_campaign({}, body_params)
+  response['id']
+end
+
+def create_banner
+  body_params = {
+    :campaignId => ENV['campaign_id'], 
+    :weight => 1,
+    :imageBannerLink => "http://beachgrooves.com/wp-content/uploads/2014/07/BeachGrooves-Logos-website2.png",
+    :url => "http://www.example.com",
+    :name => "banner #{timestamp}",
+    :bannerType => Epom::BannerType::EXTERNAL_FILE,
+    :adUnitId => 10,
+    :active => '1',
+    :placementType => Epom::PlacementType::SITE_PLACEMENT,
+    :adUnitWidth => 237,
+    :adUnitHeight => 100,
+  }
+  response = Epom::Banner.create_banner({}, body_params)
+  response['id']
+end
+
+def create_site
+  body_params = {
+    :name => "publisher_#{timestamp}",
+    :url => 'http://www.publisher.com',
+    :email => "publisher@example.com",
+    :categoryId => 2
+  }
+  response = Epom::Site.create_site({}, body_params)
+  response['id']
+end
+
+def create_zone
+  body_params = {
+    :name => "zone #{timestamp}",
+    :description => "description",
+    :siteId => ENV['site_id']
+  }
+  response = Epom::Zone.create_zone({}, body_params)
+  response['id']
+end
+
+def create_standard_placement
+  timestamp = Time.now.to_i * 1000
+  body_params = {
+    :zoneId => ENV['zone_id'],
+    :type => Epom::PlacementType::SITE_PLACEMENT,
+    :name => "placement #{timestamp}",
+    :adUnitId => 10,
+    'size.height' => 200,
+    'size.width' => 350,
+  }
+  response = Epom::Placement.create_standard_placement({}, body_params)
+  response['id']
+end
+
+
 Epom::EpomElement.login(ENV['username'], ENV['password'])
