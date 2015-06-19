e0bc29c60978c1212f31a4f8954d221e810d6bd6
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-18 22:58:00 -0400

    -       class ConfigGenerator < Rails::Generators::NamedBase +       class ConfigGenerator < Rails::Generators::Base

--- lib/generators/epom/config/config_generator.rb
+++ lib/generators/epom/config/config_generator.rb
@@ -1,9 +1,9 @@
 module Epom
-	class ConfigGenerator < Rails::Generators::NamedBase
+	class ConfigGenerator < Rails::Generators::Base
 	  source_root File.expand_path('../templates', __FILE__)
 
-	  	def copy_config_file
-	        template 'epom_config.rb', 'config/initializers/epom_config.rb'
+	  	def copy_epom_file
+	        template 'epom.rb', 'config/initializers/epom.rb'
 	   	end
 	end
 end
